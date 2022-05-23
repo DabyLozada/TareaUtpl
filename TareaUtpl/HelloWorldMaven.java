9a8f17d984459a64ed4ae1c5f3f61e8c7f8feb6e
EC2 Default User <ec2-user@ip-172-31-85-85.ec2.internal>
2022-05-23 16:47:58 +0000

    anadir nuevo archivo cloud9

---
+++ TareaUtpl/HelloWorldMaven.java
@@ -0,0 +1,10 @@
+/**
+ * Class Untitled
+ */
+public class HelloWorldMaven {
+	
+	public static void main(String[] args) {
+    	System.out.println("Hello World Maven from AWS Cloud9!");
+    	
+	}
+}
